0d0713f17ec19f5f40578ca7d0cfae775d8f21e3
Hasandecent <92886603+Hasandecent@users.noreply.github.com>
2021-11-23 01:13:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+// Question 1.Show an alert box on click on a link
+
+// function showMesg() {
+//     alert('Hello World!');
+// }
+
+// Question 2.Display some Mobile images in browser
+// function phone1() {
+//     alert("Thanks for Click here");
+// }
+
+// Question 3.Display 10 student records in table and each row should contain a delete button.
+// function std1() {
+//     var del = document.getElementById('student1');
+//     console.log(del);
+//     del.innerHTML = del.style.display = "none";
+// }
+
+// function std2() {
+//     var del = document.getElementById('student2');
+//     console.log(del);
+//     del.innerHTML = del.style.display = 'none';
+// }
+
+// function std3() {
+//     var del = document.getElementById('student3');
+//     console.log(del);
+//     del.innerHTML = del.style.display = 'none';
+// }
+
+// function std4() {
+//     var del = document.getElementById('student4');
+//     console.log(del);
+//     del.innerHTML = del.style.display = 'none';
+// }
+
+
+// Question 4. Display an image in browser. Change the picture on mouseover and set the first picture on mouseout.
+
+// function pictureOne() {
+//     var changeImg = document.getElementById('results');
+//     changeImg.src = "./img/flowers-1.jpg";
+// }
+
+// function pictureTwo() {
+//     var changeSecondImg = document.getElementById('results');
+//     changeSecondImg.src = './img/pexels-photo-206359.jpeg';
+// }
+
+// Question 5. Show a counter in browser
+
+// var couterVal = 0;
+
+// function increment() {
+//     updatedDisplay(++couterVal);
+// }
+
+// function decrease() {
+//     updatedDisplay(--couterVal);
+// }
+
+// function updatedDisplay(val) {
+//     document.getElementById('counter-label').innerHTML = val;
+// }
+
+
+// EVENTS 
+
+// Question 1. Create a signup form and display form data in your web page on submission.
